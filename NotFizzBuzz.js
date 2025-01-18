cd532fd4f8a2941f62f16c2d0724f7638e7d3fcf
Jason-pixel425 <jasonfitzg2798@gmail.com>
2025-01-18 17:24:11 +1000

    Added provided solution

--- NotFizzBuzz.js
+++ NotFizzBuzz.js
@@ -34,3 +34,24 @@ function awardBonuses(){
 }
 
 awardBonuses();
+
+// Provided solution
+// function awardBonuses(){
+//     // loop through 1 - 100
+//     for(let i = 1; i <= 100; i++){
+//         // is divisible by 3 & 5? 
+//         if(i % 15 === 0){
+//             console.log(`${i} - JACKPOT! 1 Million and a Yacht!`);
+//         } else if(i % 3 === 0){
+//             console.log(`${i} - Vacation!`);
+//         } else if(i % 5 === 0){
+//             console.log(`${i} - $100,000 bonus!`);
+//         } else {
+//             console.log(`${i} - :(`);
+//         }
+        
+//     }
+
+// }
+
+// awardBonuses();
