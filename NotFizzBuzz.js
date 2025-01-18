5f369686ea6ac05e0a27497e0efc8e5de8474cd6
Jason-pixel425 <jasonfitzg2798@gmail.com>
2025-01-18 17:14:03 +1000

    Added my solution to the NotFizzBuzz.js

--- NotFizzBuzz.js
+++ NotFizzBuzz.js
@@ -20,6 +20,17 @@ Your function's output should look something like this:
  */
 
 function awardBonuses(){
+    for (let i = 1; i <= 100; i++){
+        if (i % 3 === 0 && i % 5 === 0){
+            console.log("JACKPOT! 1 Million and a Yacht!")
+        } else if (i % 3 === 0){
+            console.log("Vacation!")
+        } else if (i % 5 === 0){
+            console.log("$100,000 bonus!")
+        } else{
+            console.log(":(")
+        }
+    }
 }
 
 awardBonuses();
