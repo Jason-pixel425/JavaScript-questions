6b1cb5ce1ed709402fbb7ebd634e00be921b61d5
Jason-pixel425 <jasonfitzg2798@gmail.com>
2025-01-18 21:18:03 +1000

    Added provided solution

--- Emojify.js
+++ Emojify.js
@@ -73,3 +73,53 @@ console.log(emojifyWord("elephant"));
 
 console.log(emojifyPhrase("I :heart: my :cat:"));
  console.log(emojifyPhrase("I :heart: my :elephant:"));
+
+
+//  Provided Solution
+
+ 
+// function emojifyWord(word){
+//     // starts or ends with colon?
+//         // no: return word 
+//     if(!word.startsWith(":") && !word.endsWith(":")) return word; 
+    
+//     // yes: remove colons 
+//     const slice = word.slice(1, -1);
+//     // exists in emoji object? 
+//     if(emojis[slice]){
+//          // yes: return emoji 
+//          return emojis[slice]
+//     } else {
+//         return slice;
+//     }   
+// }
+
+// /* 2. Write a function to find any emoji shortcodes in a phrase.
+// Your function should map over each word in the phrase, emojify any word
+// that begins and ends with a colon, then return the emojified phrase. 
+// Feel free to use your emojify function from the previous exercise!
+
+// Example input: "I :heart: my :cat:"
+// Example output: "I 💜 my 🐱"
+
+// Example input: "I :heart: my elephant"
+// Example output: "I 💜 my elephant"
+// */ 
+
+// function emojifyPhrase(phrase){
+//     // split the passed in phrase into an array
+//     // map through the array and call emojifyWord() on each word in the phrase
+//     // join the array back together as a string, and return
+//     const newPhrase = phrase.split(" ").map(word => emojifyWord(word));
+//     console.log(newPhrase.join(" "))
+    
+// }
+
+
+
+// // console.log(emojifyWord(":heart:"));
+// // console.log(emojifyWord(":flower:"));
+// // console.log(emojifyWord("elephant"));
+
+// console.log(emojifyPhrase("I :heart: my :cat:"));
+// // console.log(emojifyPhrase("I :heart: my :elephant:"));
