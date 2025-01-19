34b559ea266938229a4b9de65ce5db9888f6c4d2
Jason-pixel425 <jasonfitzg2798@gmail.com>
2025-01-19 16:56:14 +1000

    Added given solution

--- Anagram.js
+++ Anagram.js
@@ -29,3 +29,24 @@ function isAnagram(str1, str2){
 
 console.log(isAnagram("allergy", "gallery"));
 console.log(isAnagram("treasure", "measure"));
+
+// Provided solution
+
+// function isAnagram(str1, str2){
+//     // are the strings the same length? if yes return false. 
+//     if(str1.length !== str2.length) return false;
+    
+//     // split string into an array
+//     // sort
+//     // join the array back together as a string
+//     const newStr1 = str1.split('').sort().join('');
+    
+//     // repeat with second word
+//     const newStr2 = str2.split('').sort().join('');
+
+//     // are the two words equal? true or false
+//     return newStr1 === newStr2;
+// }
+
+// console.log(isAnagram("allergy", "gallery"));
+// console.log(isAnagram("treasure", "measure"));
