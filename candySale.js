332ed8e43ff950c5a06ad1689247da6ef413adda
Jason-pixel425 <jasonfitzg2798@gmail.com>
2025-01-20 17:18:30 +1000

    Update candySale.js to include my answer

--- candySale.js
+++ candySale.js
@@ -18,5 +18,8 @@ import products from "./Data/candyData.js";
 */
 
 function getSaleItems(data){
+    return data.filter(product => product.type === "sweet").map(item => {return {item: item.item, price: item.price}})
     
 };
+
+console.log(getSaleItems(products))
