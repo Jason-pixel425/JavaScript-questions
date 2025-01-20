a3f0d188a200dc5caa4e650f15f0a6243638b0cf
Jason-pixel425 <jasonfitzg2798@gmail.com>
2025-01-20 17:00:01 +1000

    Update filterArrofObj.js
    added scrimba solution. Essentialy the same as my own.

--- filterArrofObj.js
+++ filterArrofObj.js
@@ -34,3 +34,25 @@ function getFreePodcasts(data){
 }
 
 console.log(getFreePodcasts(podcasts))
+
+
+/*
+Scrimba solution
+
+function getFreePodcasts(data){
+    // filter list by paid prop
+    // use map to create a new array of objects with only the specified properties 
+    return data
+        .filter(item => item.paid === false)
+        .map(podcast => {
+            return {
+                title: podcast.title,
+                rating: podcast.rating,
+                paid: podcast.paid
+            }
+        });
+}
+
+console.log(getFreePodcasts(podcasts))
+
+*/
