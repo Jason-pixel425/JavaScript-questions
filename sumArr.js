bdd510b4c817f8f2a2ccf391711320095058b98f
Jason-pixel425 <jasonfitzg2798@gmail.com>
2025-01-20 16:09:15 +1000

    Update sumArr.js to also include scrimba solution

--- sumArr.js
+++ sumArr.js
@@ -18,3 +18,20 @@ function sumArray(arr){
 }
 
 console.log(sumArray(studentCount));
+
+/*
+    Scrimba Solution
+    const studentCount = [50,53,61,67,60,70,78,80,80,81,90,110];
+
+function sumArray(arr){
+    // initialize a new variable to hold the sum of the arr 
+    let sum = 0;
+    // loop through the studentCount arr, add each value to the sum
+    for(let i = 0; i < arr.length; i++){
+        sum += arr[i];
+    }
+    // after done looping, return the sum
+    return sum;
+}
+
+*/
