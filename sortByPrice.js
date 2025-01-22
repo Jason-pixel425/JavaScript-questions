1c80570436607f4d27b2469a8967e1a293066bcc
Jason-pixel425 <jasonfitzg2798@gmail.com>
2025-01-22 14:41:40 +1000

    Scrimba solutions added to file

--- sortByPrice.js
+++ sortByPrice.js
@@ -25,3 +25,4 @@ const listByCheapest = sortProducts(products);
 console.log(listByCheapest);
 
 
+ 
--- uniqueTags.js
+++ uniqueTags.js
@@ -25,3 +25,50 @@ function getUniqueTags(data){
 
 console.log(getUniqueTags(movieData));
 
+/*
+Scrimba solution one
+
+function getUniqueTags(data){
+    // use map to loop through the data and get a new array of tags
+    // flatten the tags array with .flat()
+    const tags = data.map(podcast => podcast.tags).flat();
+    // create a new array uniqueTags to hold the unique values
+    const uniqueTags = [];
+    // loop through the tags array
+
+    tags.forEach(tag => {
+         // is the element already in the uniqueTags arr? 
+         if(!uniqueTags.includes(tag)){
+             uniqueTags.push(tag)
+         }
+    })
+       
+    return uniqueTags;
+}
+
+console.log(getUniqueTags(mediaData));
+
+Scrimba solution two
+
+function getUniqueTags(data){
+    const tags = data.map(podcast => podcast.tags).flat();
+    const uniqueTags = {};
+    
+    // filter tags arr
+    return tags.filter(tag => {
+         // look up the tag in the uniqueTags obj
+         if(!uniqueTags[tag]){
+             uniqueTags[tag] = true;
+            // if it's not the, we have a unique item 
+            // put the item in our object with a value of true
+            return true;
+         }
+         return false;
+    })
+
+}
+
+console.log(getUniqueTags(mediaData));
+
+
+*/
