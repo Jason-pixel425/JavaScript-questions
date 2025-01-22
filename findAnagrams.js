a84824333c4a333c138478d90a22f974c55498f0
Jason-pixel425 <jasonfitzg2798@gmail.com>
2025-01-22 21:01:00 +1000

    Update findAnagrmas.js
    Added my and Scimba solution to file

--- findAnagrams.js
+++ findAnagrams.js
@@ -37,7 +37,42 @@ const anagrams = [
 ]
 
 function isAnagramInArray(anagram, arr){
-
+    const sortedAnagram = anagram.replace(/\s/g, '').toLowerCase().split('').sort().join('');
+    const sortedAnagramLength = sortedAnagram.length
+    return arr.filter(str => {
+        const sortedString = str.replace(/\s/g, '').toLowerCase();
+        if (sortedAnagramLength !== sortedString.length){
+            return false
+        } else {
+            return sortedAnagram === sortedString.split('').sort().join('')
+        }
+        
+    })
 }
 
 console.log(isAnagramInArray("Bob Ziroll Scrimba Teacher", anagrams));
+
+
+/*
+Scrimba solution
+
+function sortPhrase(phrase){
+    return phrase.toLowerCase().split('').sort().join('').trim();
+}
+
+function isAnagramInArray(anagram, arr){
+    // use .filter()
+    // use sortPhrase() to sort both the input phrase and the current phrase in the array
+    // compare the two words to return true or false
+    return arr.filter(item => {
+        const word1 = sortPhrase(anagram);
+        const word2 = sortPhrase(item);
+        
+        return word1 === word2;
+    })
+}
+
+console.log(isAnagramInArray("Tom Chant Scrimba Teacher", anagrams));
+//console.log(isAnagramInArray("treat", ["tater", "tree", "teart", "tetra", "heart", "hamster"]))
+
+*/
